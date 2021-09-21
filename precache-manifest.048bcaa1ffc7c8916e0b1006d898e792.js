f7eb851615612fbd4266f07e11417d2554a4e42f
Ranvir Prasad <ranvir.prasad@gmail.com>
2021-09-21 06:49:18 +0530

    Updates

---
+++ precache-manifest.048bcaa1ffc7c8916e0b1006d898e792.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b12a5cdc341e2783b877863c5787045",
+    "url": "/gisfrontend/index.html"
+  },
+  {
+    "revision": "76adbf704df3b914a43e",
+    "url": "/gisfrontend/static/css/2.016d901b.chunk.css"
+  },
+  {
+    "revision": "4efa8697f18096b1fdfe",
+    "url": "/gisfrontend/static/css/main.dab032e9.chunk.css"
+  },
+  {
+    "revision": "76adbf704df3b914a43e",
+    "url": "/gisfrontend/static/js/2.ef0482f4.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/gisfrontend/static/js/2.ef0482f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b597f1c09829b190a7",
+    "url": "/gisfrontend/static/js/3.11f9259d.chunk.js"
+  },
+  {
+    "revision": "4efa8697f18096b1fdfe",
+    "url": "/gisfrontend/static/js/main.e08851d4.chunk.js"
+  },
+  {
+    "revision": "f0429ae91ee8657379e9",
+    "url": "/gisfrontend/static/js/runtime-main.f6839d16.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/gisfrontend/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/gisfrontend/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/gisfrontend/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/gisfrontend/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/gisfrontend/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gisfrontend/static/media/logo.06e73328.svg"
+  }
+]);
